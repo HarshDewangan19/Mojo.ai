492a2f523cf094eba383ec67e54bc0c2fd86892f
Harsh Dewangan <harsh101dewangan@gmail.com>
2025-03-08 20:14:43 +0530

    requirements added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'Mojo.ai',
+    version = '0.0.0',
+    author = 'Harsh',
+    author_email = 'harsh101dewangan@gmail.com',
+    packages = find_packages(),
+    install_requires = []
+)
